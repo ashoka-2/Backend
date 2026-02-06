366c068290faf2e0b211d78d1ce6052384c077c4
Ashok Kumar <choudharyashok1230@gmail.com>
2026-02-06 21:13:21 +0530

    auth with token day 1

---
+++ token/src/routes/auth.routes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const userModel = require('../models/user.model');
+
+const jwt = require('jsonwebtoken');
+const authRouter = express.Router();
+
+
+authRouter.post('/register', async (req, res) => {
+    const { email, name, password } = req.body;
+
+    const isUserAlreadyExists = await userModel.findOne({ email });
+
+    if (isUserAlreadyExists) {
+        return res.status(400).json({
+            message: "User already exists with this email",
+
+        })
+    }
+
+    const user = await userModel.create({
+        email, password, name
+    })
+
+
+    const token = jwt.sign(
+        {
+            id: user._id,
+            email: user.email
+        },
+        process.env.JWT_SECRET,
+    );
+
+    res.cookie("token", token, )
+
+    res.status(201).json({
+        message: "User registered successfully",
+        user,
+        token
+    })
+
+
+
+})
+
+
+module.exports = authRouter;
